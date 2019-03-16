285bc6fabd539b669ad87e32c2a2cd1549a404f9
sriram <sriramram1995@gmail.com>
2019-03-16 17:32:00 +0530

    dependcies added

--- src/store/storeDev.js
+++ src/store/storeDev.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
 import ReduxThunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from '../reducers'
 
 export default function configureStore(initialState = {}) {
-  const middlewares = [ReduxThunk]
+  const middlewares = [ReduxThunk, logger]
   const enhancers = [
     applyMiddleware(...middlewares),
     // other store enhancers if any
@@ -13,7 +14,7 @@ export default function configureStore(initialState = {}) {
     // other compose enhancers if any
     // Specify here other options if needed
   })
-  const store = createStore(rootReducer, initialState, composeEnhancers(...enhancers))
+  const store = createStore(rootReducer, initialState, composeEnhancers(...enhancers));
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('../reducers', () => {
